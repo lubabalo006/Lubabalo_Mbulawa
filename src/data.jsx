93b11d8613fbe6b8c5492c187cd3b0267cf05575
lubabalo006 <137266437+lubabalo006@users.noreply.github.com>
2024-01-13 17:05:00 +0200

    data update

--- src/data.jsx
+++ src/data.jsx
@@ -2,17 +2,17 @@ export const Projects = [
 {
     "name": "Book-connect",
     "github": "https://github.com/lubabalo006/lubabalo006/tree/main/Book-connect",
-    "image": "src/images/project_images/Book-connect_Screenshot 2024-01-10 145123.png"
+    "image": "https://i.postimg.cc/vZS7Bz44/Book-connect-Screenshot-2024-01-10-145123.png"
 },
 {
     "name": "AirBnB",
     "github": "https://github.com/lubabalo006/LUBMBU219_FTC2306_GroupB_LubabaloMbulawa_DWA1/tree/main/airbnb_clone",
-    "image": "src/images/project_images/AirBnB_Screenshot 2024-01-10 144639.png"
+    "image": "https://i.postimg.cc/C18s9qLz/Air-Bn-B-Screenshot-2024-01-10-144639.png"
 },
 {
     "name": "Meme generator",
     "github": "https://github.com/lubabalo006/LUBMBU219_FTC2306_GroupB_LubabaloMbulawa_DWA1/tree/main/Meme_generator",
-    "image": "src/images/project_images/Meme_generator_Screenshot 2024-01-10 152141.png"
+    "image": "https://i.postimg.cc/0j8KZM6g/Meme-generator-Screenshot-2024-01-10-152141.png"
 }
 ]
 
